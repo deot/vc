3f5afbdca8499e2828d5b977b8afce2e3bac89d7
Mr.Z <zrd0921@qq.com>
2023-10-21 18:02:47 +0800

    feat: [components] style theme supported

--- packages/preload.ts
+++ packages/preload.ts
@@ -1,1 +1,27 @@
 import "./components/style";
+
+document.body.style.background = `var(--vc-background-color)`;
+document.body.style.color = `var(--vc-foreground-color)`;
+
+const el = document.createElement('div');
+
+el.style.position = 'fixed';
+el.style.top = '0';
+el.style.right = '0';
+el.style.cursor = 'pointer';
+document.body.appendChild(el);
+
+const key = "data-vc-theme";
+let theme = document.body.getAttribute(key) || 'light';
+
+const flip = (v: string) => (v === 'dark' ? 'light' : 'dark');
+const setTheme = () => {
+	document.body.setAttribute(key, theme);
+	el.innerHTML = theme;
+};
+
+setTheme();
+el.addEventListener('click', () => {
+	theme = flip(theme);
+	setTheme();
+});
